ec7f0ae3c25396dda9c142ff9f6489f3c45dfcd0
JhonatanNeves <nevesjn2@gmail.com>
2022-02-15 17:48:57 -0300

    att

--- Program.cs
+++ Program.cs
@@ -10,7 +10,37 @@ namespace Projeto
             Console.WriteLine("1- Inserir novo aluno");
             Console.WriteLine("2- Listar alunos");
             Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
             Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch(opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: adicionar aluno
+                        break;
+                    case "2":
+                        //TODO: listar alunos
+                        break;
+                    case "3":
+                        //TODO: calcular média geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                Console.WriteLine("Informe a opção desejada:");
+                Console.WriteLine("2- Listar alunos");
+                Console.WriteLine("1- Inserir novo aluno");
+                Console.WriteLine("3- Calcular média geral");
+                Console.WriteLine("X- Sair");
+                Console.WriteLine();
+
+                opcaoUsuario = Console.ReadLine();
+            }
         }
     }
 }
